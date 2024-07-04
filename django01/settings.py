b524a5781ff7b1a2324ae70b40380d3234b2639b
gmh570154 <570154262@qq.com>
2024-07-04 18:03:57 +0800

    fix: 删除多余代码

--- django01/settings.py
+++ django01/settings.py
@@ -83,13 +83,6 @@ WSGI_APPLICATION = 'django01.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认
